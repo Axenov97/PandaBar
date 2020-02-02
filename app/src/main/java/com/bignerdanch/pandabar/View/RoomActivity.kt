3addce722a1a3a7c080eb191949eb813e2d951a8
Dmitrii <dimka180497@gmail.com>
2020-02-02 21:58:34 +0300

    косяк в Модели

--- app/src/main/java/com/bignerdanch/pandabar/View/RoomActivity.kt
+++ app/src/main/java/com/bignerdanch/pandabar/View/RoomActivity.kt
@@ -2,7 +2,9 @@ package com.bignerdanch.pandabar.View
 
 import androidx.fragment.app.Fragment
 import com.bignerdanch.pandabar.Contract.Single
+import com.bignerdanch.pandabar.R
 
 class RoomActivity : Single() {
     override fun createFragment(): Fragment = RoomFragment()
+
 }
